5a24346891f668b9913c2056829a0bfb2f9da1b7
mark <mark.lodato@deepgram.com>
2022-12-07 09:03:54 -0500

    Day 1 Part 2
    I was going to call `sort_unstable` on a `[u64; 3]` which is [somewhat
    optimized][0] (and extends to various `N`s) but I decided to go with
    [this][1] because other folks on the internet liked it and who am I to
    argue?!
    
    [0]: https://godbolt.org/z/doT8nGna9
    [1]: https://users.rust-lang.org/t/solved-best-way-to-find-largest-three-values-in-unsorted-slice/34754

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -2,71 +2,156 @@ pub fn part1(input: &str) -> u64 {
     input
         .lines()
         .scan(0, |calorie_count, line| {
-            *calorie_count = new_calories(*calorie_count, line);
+            let new_calories = if line.is_empty() {
+                0
+            } else {
+                let calories: u64 = line.parse().expect("Calories must be a number");
+                *calorie_count + calories
+            };
+            *calorie_count = new_calories;
             Some(*calorie_count)
         })
         .max()
         .unwrap_or_default()
 }
 
-fn new_calories(current_calories: u64, line: &str) -> u64 {
-    if line.is_empty() {
-        0
-    } else {
-        let calories: u64 = line.parse().expect("Calories must be a number");
-        current_calories + calories
+pub fn part2(input: &str) -> u64 {
+    // Could use an array but this keeps the sort order clearer.
+    let mut smallest = 0;
+    let mut middle = 0;
+    let mut biggest = 0;
+
+    let mut elf_calories = 0;
+
+    for line in input.lines() {
+        if line.is_empty() {
+            if elf_calories > biggest {
+                smallest = middle;
+                middle = biggest;
+                biggest = elf_calories;
+            } else if elf_calories > middle {
+                smallest = middle;
+                middle = elf_calories;
+            } else if elf_calories > smallest {
+                smallest = elf_calories;
+            }
+            elf_calories = 0;
+        } else {
+            let calories: u64 = line.parse().expect("Calories must be a number");
+            elf_calories += calories;
+        }
     }
+    if elf_calories > biggest {
+        smallest = middle;
+        middle = biggest;
+        biggest = elf_calories;
+    } else if elf_calories > middle {
+        smallest = middle;
+        middle = elf_calories;
+    } else if elf_calories > smallest {
+        smallest = elf_calories;
+    };
+
+    smallest + middle + biggest
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn one_elf_one_line() {
-        let file = "1000\n";
-        let max_calories = part1(file);
+    mod part1 {
+        use super::*;
 
-        assert_eq!(max_calories, 1000);
+        #[test]
+        fn one_elf_one_line() {
+            let file = "1000\n";
+            let max_calories = part1(file);
+
+            assert_eq!(max_calories, 1000);
+        }
+
+        #[test]
+        fn one_elf_multiple_lines() {
+            let file = "1000\n1000\n";
+            let max_calories = part1(file);
+
+            assert_eq!(max_calories, 2000);
+        }
+
+        #[test]
+        fn two_elves_one_line_max_first() {
+            let file = "2000\n\n1000\n";
+            let max_calories = part1(file);
+
+            assert_eq!(max_calories, 2000);
+        }
+
+        #[test]
+        fn two_elves_one_line_max_last() {
+            let file = "1000\n\n2000\n";
+            let max_calories = part1(file);
+
+            assert_eq!(max_calories, 2000);
+        }
+
+        #[test]
+        fn two_elves_multiple_lines_max_first() {
+            let file = "1000\n1000\n\n1000\n";
+            let max_calories = part1(file);
+
+            assert_eq!(max_calories, 2000);
+        }
+
+        #[test]
+        fn two_elves_multiple_lines_max_last() {
+            let file = "1000\n\n1000\n1000\n";
+            let max_calories = part1(file);
+
+            assert_eq!(max_calories, 2000);
+        }
     }
 
-    #[test]
-    fn one_elf_multiple_lines() {
-        let file = "1000\n1000\n";
-        let max_calories = part1(file);
+    mod part2 {
+        use super::*;
 
-        assert_eq!(max_calories, 2000);
-    }
+        #[test]
+        fn one_elf_one_line() {
+            let file = "1000\n";
+            let sum_max_3 = part2(file);
 
-    #[test]
-    fn two_elves_one_line_max_first() {
-        let file = "2000\n\n1000\n";
-        let max_calories = part1(file);
+            assert_eq!(sum_max_3, 1000);
+        }
 
-        assert_eq!(max_calories, 2000);
-    }
+        #[test]
+        fn one_elf_multiple_lines() {
+            let file = "1000\n1000\n";
+            let sum_max_3 = part2(file);
 
-    #[test]
-    fn two_elves_one_line_max_last() {
-        let file = "1000\n\n2000\n";
-        let max_calories = part1(file);
+            assert_eq!(sum_max_3, 2000);
+        }
 
-        assert_eq!(max_calories, 2000);
-    }
+        #[test]
+        fn two_elves_one_line() {
+            let file = "2000\n\n1000\n";
+            let sum_max_3 = part2(file);
 
-    #[test]
-    fn two_elves_multiple_lines_max_first() {
-        let file = "1000\n1000\n\n1000\n";
-        let max_calories = part1(file);
+            assert_eq!(sum_max_3, 3000);
+        }
 
-        assert_eq!(max_calories, 2000);
-    }
+        #[test]
+        fn two_elves_two_lines() {
+            let file = "1000\n1000\n\n1000\n1000\n";
+            let sum_max_3 = part2(file);
 
-    #[test]
-    fn two_elves_multiple_lines_max_last() {
-        let file = "1000\n\n1000\n1000\n";
-        let max_calories = part1(file);
+            assert_eq!(sum_max_3, 4000);
+        }
 
-        assert_eq!(max_calories, 2000);
+        #[test]
+        fn four_elves_max_013() {
+            let file = "1000\n\n2000\n\n500\n\n3000";
+            let sum_max_3 = part2(file);
+
+            assert_eq!(sum_max_3, 6000);
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,6 @@ use aoc_2022::day1;
 const DAY_1_INPUT: &str = include_str!("./day1/input.txt");
 
 fn main() {
-    println!("Day 1 - {}", day1::part1(DAY_1_INPUT));
+    println!("Day 1 Part 1 - {}", day1::part1(DAY_1_INPUT));
+    println!("Day 1 Part 2 - {}", day1::part2(DAY_1_INPUT));
 }
